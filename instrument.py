dda4437a867d8a7e1259c8d7e028684af6ea8c81
surajn581 <surajn581@gmail.com>
2025-07-30 02:56:20 +0530

    updated instrument to be able to use both kind of curves, dsc and fwd

--- instrument.py
+++ instrument.py
@@ -6,8 +6,19 @@ class Instrument:
         self.type = instrument_type
 
     def depends_on(self):
-        return ["DiscountCurve"]
+        if self.type == "bond":
+            return ["DiscountCurve"]
+        elif self.type == "futures":
+            return ["ForwardCurve"]
+        else:
+            return []
 
     def Price(self):
-        dsc = Env.get("DiscountCurve")
-        return 100.0 if dsc == "USD_1" else 95.0
+        if self.type == "bond":
+            dsc = Env.get("DiscountCurve")
+            return 100.0 if dsc == "USD_1" else 95.0
+        elif self.type == "futures":
+            fwd = Env.get("ForwardCurve")
+            return 200.0 if fwd == "FW_1" else 190.0
+        else:
+            return 0.0
