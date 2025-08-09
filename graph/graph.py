5f6e0665d9e364c174239229fdb92885fbda51dd
surajn581 <surajn581@gmail.com>
2025-08-09 23:15:28 +0530

    moved the node logic into GraphNode class and using a setter that can set node values marked with settable flag.

--- graph/graph.py
+++ graph/graph.py
@@ -1,13 +1,13 @@
 import functools
 from lib.logging import logger
 
-def _getDependency(self, name):
-    if not self._dependencies[name]:
-        return []
-    return list( [_name] + _getDependency(self, _name) for _name in self._dependencies[name] )[0]
+# def _getDependency(self, name):
+#     if not self._dependencies[name]:
+#         return []
+#     return list( [_name] + _getDependency(self, _name) for _name in self._dependencies[name] )[0]
 
-def getDependency(self, name):
-    return set(_getDependency(self, name))
+# def getDependency(self, name):
+#     return set(_getDependency(self, name))
 
 def updateDependencies(self, name):
     if not self._current_stack:
@@ -19,37 +19,65 @@ def updateDependencies(self, name):
         self._dependents[name].add(parent)
         stack_length-=1
 
-def graph_node(func):
-    """Decorator to mark a method as a graph node with caching + dependency tracking."""
-    name = func.__name__
+class GraphNode:
+    """Graph Node"""
+    def __init__(self, func, settable=False):
+        self.func = func
+        self.name = func.__name__
+        self.settable = settable
+        self._is_graph_node = True
 
-    @functools.wraps(func)
-    def wrapper(self, *args, **kwargs):
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
 
-        # update the dependency as soon as we enter the call stack
-        updateDependencies(self, name)
+        # Return a callable wrapper so g.A() still works
+        @functools.wraps(self.func)
+        def caller(*args, **kwargs):
 
-        # If cached, return from cache
-        logger.debug(f'looking for {name} in cache')
-        if name in self._cache:
-            logger.debug(f'found {name} in cache')
-            return self._cache[name]
+            # update the dependency as soon as we enter the call stack
+            # TODO noticed a bug when nested deps are not captured incase one of the nested dep is already cached.
+            # eg:   if F > E > D > B > A and E is already in cache then when calling F()
+            #       the call only marks F: {E}
+            updateDependencies(instance, self.name)
 
-        # Track call stack to discover dependencies
-        self._current_stack.append(name)
-        logger.debug(f'{"...."*len(self._current_stack)}entered stack for {name} | stack: {self._current_stack}')
-        result = func(self, *args, **kwargs)        
-        self._current_stack.pop()
-        logger.debug(f'{"...."*len(self._current_stack)}exitied stack for {name} | stack: {self._current_stack}')
+            # If cached, return value
+            logger.debug(f'[GraphNode] looking for {self.func} in cache')
+            if self.name in instance._cache:
+                logger.debug(f'[GraphNode] found {self.func} in cache')
+                return instance._cache[self.name]
 
-        # Cache result
-        logger.debug(f'adding {name}: {result} in cache')
-        self._cache[name] = result
-        return result
+            # Track call stack for dependency discovery
+            instance._current_stack.append(self.name)
+            logger.debug(f'[GraphNode] {"...."*len(instance._current_stack)}entered stack for {self.func} | stack: {instance._current_stack}')
+            result = self.func(instance, *args, **kwargs)
+            instance._current_stack.pop()
+            logger.debug(f'[GraphNode] {"...."*len(instance._current_stack)}exitied stack for {self.func} | stack: {instance._current_stack}')
 
-    wrapper._is_graph_node = True
-    return wrapper
+            # Cache result
+            logger.debug(f'[GraphNode] adding {self.func}: {result} in cache')
+            instance._cache[self.name] = result
+            return result
 
+        # Attach .set() if settable
+        if self.settable:
+            def setter(value):
+                instance._cache[self.name] = value
+                instance.invalidate(self.name)
+                logger.debug(f'[GraphNode] {self.func} invalidated')
+            caller.set = setter
+
+        return caller
+
+
+def graph_node(settable=False):
+    """ graph_node decorator
+        input:
+        ------
+        settable: bool, if True, the node can be set with syntax: node.attr(value)
+    """
+    def decorator(func): return GraphNode(func, settable=settable)
+    return decorator
 
 class Graph:
     def __init__(self):
@@ -59,10 +87,9 @@ class Graph:
         self._dependents = {}     # node -> set of dependents
         self._current_stack = []  # for dependency discovery
 
-        # Initialize dependencies dict for all graph nodes
-        for attr in dir(self):
-            method = getattr(self, attr)
-            if callable(method) and getattr(method, "_is_graph_node", False):
+        # Scan class attributes without triggering descriptors
+        for attr, value in vars(self.__class__).items():
+            if isinstance(value, GraphNode):
                 self._dependencies[attr] = set()
                 self._dependents[attr] = set()
 
@@ -75,50 +102,42 @@ class Graph:
         for dep in self._dependents.get(node, []):
             self.invalidate(dep)
 
-    def set_value(self, node, value):
-        """Set value of a leaf node and invalidate its dependents."""
-        self._cache[node] = value
-        self.invalidate(node)
 
-# Example graph class
 class MyGraph(Graph):
     def __init__(self):
         logger.debug('[MyGraph] __init__ start')
-        self._A = 10
         super().__init__()
         logger.debug('[MyGraph] __init__ end')
 
-    @graph_node
+    @graph_node(settable=True)
     def A(self):
-        logger.debug('graph_node A called')
-        return self._A
+        logger.debug('[MyGraph] graph_node A called')
+        return 10
 
-    @graph_node
+    @graph_node()
     def B(self):
-        logger.debug('graph_node B called')
+        logger.debug('[MyGraph] graph_node B called')
         return self.A() ** 2
 
-    @graph_node
+    @graph_node()
     def C(self):
-        logger.debug('graph_node C called')
+        logger.debug('[MyGraph] graph_node C called')
         return self.B() + self.A()
     
-    @graph_node
+    @graph_node()
     def D(self, value):
+        logger.debug('[MyGraph] graph_node D called')
         return value*self.B()
     
-    @graph_node
+    @graph_node()
     def E(self):
+        logger.debug('[MyGraph] graph_node E called')
         return self.D( self.A() ) * 2
     
-    @graph_node
+    @graph_node()
     def F(self):
+        logger.debug('[MyGraph] graph_node F called')
         if self.A() > 5:
             return self.E()*1.5
         else:
             return 14
-
-    def set_A(self, value):
-        self._A = value
-        self.invalidate("A")
-        logger.debug('graph_node A invalidated')
