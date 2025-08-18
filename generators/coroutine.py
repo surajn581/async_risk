bbaf0e1fec9c601c63d1111c09ccc3968fda108a
surajn581 <surajn581@gmail.com>
2025-08-19 04:15:08 +0530

    Comments added

--- generators/coroutine.py
+++ generators/coroutine.py
@@ -1,6 +1,10 @@
 import functools
 
 class Coroutine:
+    '''
+    Wrapper class to solve to take care of the priming of the generator
+    and solve the double send problem with generators that accept input
+    '''
     def __init__(self, task, *args, **kwargs):
         self.func = task
         self.args = args
@@ -9,6 +13,10 @@ class Coroutine:
         self.__iter__ = self.task.__iter__
 
     def send(self, input):
+        '''
+        send method that calls next() on the generator before sending the input
+        to address the double send problem
+        '''
         next(self.task)
         res = self.task.send(input)        
         return res
@@ -20,6 +28,9 @@ class Coroutine:
         return f'{self.__class__.__name__}({self.func.__name__}, *{self.args}, **{self.kwargs})'
     
 def coroutine(func):
+    '''
+    decorator that returns a Coroutine object
+    '''
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         return Coroutine(func, *args, **kwargs)
