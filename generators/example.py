b19848798e98c7e7c016adcf39ef45d46b0626b6
surajn581 <surajn581@gmail.com>
2025-08-19 09:53:56 +0530

    added examples of coroutines that can call other coroutines

--- generators/example.py
+++ generators/example.py
@@ -1,3 +1,4 @@
+import time
 from coroutine import coroutine
 from event_loop import EventLoop
 from lib.logging import logger
@@ -13,10 +14,32 @@ def sleep(duration):
             return f'slept for ~ {now-start} seconds'
         yield
 
-def main():
+@coroutine
+def work(input):
+    '''
+    coroutine that yields from another coroutine
+    '''
+    yield from sleep(input)
+    yield input
+
+def example1():
     with Timer('Creating and running all the sleep tasks'):
         loop = EventLoop([sleep(i) for i in range(5)])
         loop.run()
 
     for task, return_value in loop.tasks.items():
         logger.info(f'task: {task} | returned: {return_value}')
+
+def example2():    
+    with Timer('Creating and running all the tasks'):
+        loop = EventLoop([work(i) for i in range(5)])
+        loop.run()
+
+    for task, return_value in loop.tasks.items():
+        logger.info(f'task: {task} | returned: {return_value}')
+
+def main():
+    examples = [example1, example2]
+    for i, example in enumerate(examples):
+        logger.info(f"{'='*50} Example: {i} {'='*50}")
+        example()
