42e9ea0dc9668857d225b175b177f5ac31f22c5c
surajn581 <surajn581@gmail.com>
2025-08-26 03:38:20 +0530

    Pipe based process communication and async processing in forwareder and worker

--- rxpy/process_utils.py
+++ rxpy/process_utils.py
@@ -1,3 +1,4 @@
+import asyncio
 import multiprocessing as mp
 import cloudpickle
 from rx.subject import Subject
@@ -14,39 +15,63 @@ class SubprocessManager:
     def __init__(self, name=''):
         self.name = f'{name}:{str(id(self))}' if name else str(id(self))
         self.procs = []
-        self.queues = []
+        self.pipes = []
 
     def __repr__(self):
         return self.name
 
     def get_worker(self):
         logger.info(f'[{self.__class__.__name__}] creating subprocess')
-        in_q, out_q = mp.Queue(), mp.Queue()
-        p = mp.Process(target=self.worker_loop, args=(in_q, out_q))
+        parent_conn, child_conn = mp.Pipe(duplex=True)
+        p = mp.Process(target=self.worker_loop, args=(child_conn,))
         p.start()
+        child_conn.close()
         self.procs.append(p)
-        self.queues.append((in_q, out_q))
+        self.pipes.append(parent_conn)
         logger.info(
-            f'[{self.__class__.__name__}] subprocess: {p.pid} and queues: {[in_q, out_q]} created')
-        return in_q, out_q
+            f'[{self.__class__.__name__}] subprocess: {p.pid} and pipe created')
+        return parent_conn
 
     @staticmethod
-    def worker_loop(in_q, out_q):
+    def worker_loop(conn):
+        asyncio.run(SubprocessManager.worker_async_loop(conn))
+
+    @staticmethod
+    async def worker_async_loop(conn):
         while True:
-            msg = in_q.get()
+            try:
+                # recv is blocking, run in thread
+                msg = await asyncio.to_thread(conn.recv)
+            except EOFError:
+                break
+
             if msg is None:
                 break
+
             func_bytes, data = msg
-            func = cloudpickle.loads(func_bytes)
             try:
-                result = func(data)
-                out_q.put(result)
+                func = cloudpickle.loads(func_bytes)
+                if asyncio.iscoroutinefunction(func):
+                    result = await func(data)
+                else:
+                    # run sync func in executor to avoid blocking
+                    loop = asyncio.get_running_loop()
+                    result = await loop.run_in_executor(None, func, data)
+                await asyncio.to_thread(conn.send, result)
             except Exception as e:
-                out_q.put(e)
+                # Send exception back
+                await asyncio.to_thread(conn.send, e)
+
+        conn.close()
 
     def shutdown(self):
-        for in_q, _ in self.queues:
-            in_q.put(None)
+        for conn in self.pipes:
+            try:
+                conn.send(None)  # signal to stop worker
+                conn.close()
+            except Exception:
+                pass
+
         for p in self.procs:
             logger.info(
                 f'[{self.__class__.__name__}] Closing process: {p.pid}')
@@ -74,20 +99,25 @@ class Execute:
             return out_stream
 
         elif location == Location.SUBPROC:
-            # Run remotely
+            # Run remotely with async Pipe
             self.manager = SubprocessManager(func.__name__)
             self._managers.append(self.manager)
-            in_q, out_q = self.manager.get_worker()
+            conn = self.manager.get_worker()
             func_bytes = cloudpickle.dumps(func)
             out_stream = Subject()
 
-            def forwarder(x):
-                in_q.put((func_bytes, x))
-                result = out_q.get()
+            async def forwarder_async(x):
+                conn.send((func_bytes, x))
+                # await result without blocking using to_thread
+                result = await asyncio.to_thread(conn.recv)
                 if isinstance(result, Exception):
-                    logger.info("[Subprocess error] {}".format(result))
+                    logger.info(f"[Subprocess error] {result}")
                 else:
                     out_stream.on_next(result)
+
+            # Wrap the async forwarder in a sync function for Rx subscription
+            def forwarder(x):
+                asyncio.create_task(forwarder_async(x))
 
             input.subscribe(forwarder, scheduler=scheduler)
             return out_stream
